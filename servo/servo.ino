4e63195ccb02ff92616c22f6307ffebc3acb8fea
todeilatiberia <todeilatiberia@gmail.com>
2016-12-31 00:58:11 +0200

    -
    -

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,5 +1,9 @@
-
 #include <Servo.h>
+
+
+
+
+
  
 Servo myservo;  // create servo object to control a servo
                 // a maximum of eight servo objects can be created
@@ -8,21 +12,23 @@ int pos = 0;    // variable to store the servo position
  
 void setup()
 {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(9);
+   // attaches the servo on pin 9 to the servo object
 }
  
  
 void loop()
 {
+
   for(pos = 130; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees
   {
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+delay (20);//waits 15ms for the servo to reach the position
   }
   for(pos = 50; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees
   {                                  // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(20);                       // waits 15ms for the servo to reach the position
+   delay(20);                       // waits 15ms for the servo to reach the position
   }
   
 }
